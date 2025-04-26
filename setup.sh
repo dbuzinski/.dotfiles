41d2a6d70547e5d5bd8fdc820d74ce1191306160
David Buzinski <davidbuzinski@gmail.com>
2025-04-26 22:13:12 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -52,6 +52,7 @@ fi
 # Linux
 if [ "$(uname)" == "Linux" ]; then
   # Install with apt
+  echo "Installing system dependencies"
   export DEBIAN_FRONTEND=noninteractive
   if [ -x "$(command -v apt)" ]; then
     sudoIfAvailable apt-get update
@@ -63,52 +64,7 @@ if [ "$(uname)" == "Linux" ]; then
     # Install tmux
     sudoIfAvailable apt-get install -y tmux
     sudoIfAvailable apt-get install -y gcc
-  # Install with pacman
-  elif [ -x "$(command -v pacman)" ]; then
-      sudoIfAvailable pacman -Sy which
-    # Check for git and install if we don't have it
-    if test ! $(which git); then
-      sudoIfAvailable pacman -Sy --noconfirm git
-    fi
-    sudoIfAvailable pacman -Sy --noconfirm curl
-    # Install tmux
-    sudoIfAvailable pacman -Sy --noconfirm tmux
-    sudoIfAvailable pacman -Sy --noconfirm gcc
-  # Install with dnf
-  elif [ -x "$(command -v dnf)" ]; then
-    # Check for git and install if we don't have it
-    sudoIfAvailable dnf install -y which
-    if test ! $(which git); then
-      sudoIfAvailable dnf install -y git
-    fi
-    sudoIfAvailable dnf install -y curl
-    # Install tmux
-    sudoIfAvailable dnf install -y http://galaxy4.net/repo/galaxy4-release-8-current.noarch.rpm
-    sudoIfAvailable dnf install -y tmux
-    sudoIfAvailable dnf install -y gcc
-  # Install with yum
-  elif [ -x "$(command -v yum)" ]; then
-    sudoIfAvailable yum install -y which
-    # Check for git and install if we don't have it
-    if test ! $(which git); then
-      sudoIfAvailable yum install -y git
-    fi
-    sudoIfAvailable yum install -y curl
-    # Install tmux
-    sudoIfAvailable yum install -y http://galaxy4.net/repo/galaxy4-release-8-current.noarch.rpm
-    sudoIfAvailable yum install -y tmux
-    sudoIfAvailable yum install -y gcc
-  # Install with zypper
-  elif [ -x "$(command -v zypper)" ]; then
-    # Check for git and install if we don't have it
-    sudoIfAvailable zypper install -y which tar gzip
-    if test ! $(which git); then
-      sudoIfAvailable zypper install -y git
-    fi
-    sudoIfAvailable zypper install -y curl
-    # Install tmux
-    sudoIfAvailable zypper install -y tmux
-    sudoIfAvailable zypper install -y gcc
+    sudoIfAvailable apt-get install -y ripgrep
   # Catch unsupported Linux distros
   else
     echo "Unsupported Linux distro"
@@ -116,35 +72,42 @@ if [ "$(uname)" == "Linux" ]; then
   fi
 
   # Install neovim if not already installed
+  echo "Installing neovim"
   if ! command -v nvim &> /dev/null; then
-    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
     sudoIfAvailable rm -rf /opt/nvim
-    sudoIfAvailable tar -C /opt -xzf nvim-linux64.tar.gz
+    sudoIfAvailable tar -C /opt -xzf nvim-linux64-x86_64.tar.gz
     sudoIfAvailable ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
   fi
 fi
 
 # Install kickstart neovim if no existing neovim config
+echo "Installing kickstart neovim"
 if [ ! -d "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim ]; then
   git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 fi
 
 
 # Install tmux plugin manager if not already installed
+echo "Installing tpm"
 if [ ! -d $HOME/.tmux/plugins/tpm ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 # Copy tmux config to $HOME/.tmux.conf
+echo "Setting up tmux config"
 cp tmux/tmux.conf $HOME/.tmux.conf
 
 # Copy neovim config to $HOME/.config/nvim/init.lua
+echo "Setting up neovim config"
 cp nvim/init.lua $HOME/.config/nvim/init.lua
 
 # Add neovim plugin configuration
+echo "Adding neovim plugins"
 cp -r nvim/lua $HOME/.config/nvim/
 
 # Add neovim alias to .bashrc or .zshrc
+echo "Updating .bashrc"
 if [ -f $HOME/.bashrc ]; then
   echo "alias vim=nvim" >> $HOME/.bashrc
 fi
